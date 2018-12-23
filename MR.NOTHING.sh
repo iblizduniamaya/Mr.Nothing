5c1afa31813f83515880f3d51ae1c28e7dd94417
iblizduniamaya <46085310+iblizduniamaya@users.noreply.github.com>
2018-12-23 14:37:01 +0000

    Add files via upload

---
+++ MR.NOTHING.sh
@@ -0,0 +1,251 @@
+#!/bin/bash/system
+#NoRecode
+#NoCopas
+#MikirPkeOtak
+#JanganMikirPkeDengkulgblk
+#HargaiHasilKodingOrangBangsat
+clear
+#version 1.0
+#31/10/2018
+
+clear
+# Variables
+b='\033[1m'
+u='\033[4m'
+bl='\E[30m'
+r='\E[31m'
+g='\E[32m'
+bu='\E[34m'
+m='\E[35m'
+c='\E[36m'
+w='\E[37m'
+endc='\E[0m'
+enda='\033[0m'
+blue='\e[1;34m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+
+figlet "MR. NOTHING" | lolcat
+echo  "★=========================«[+]»===========================★" | lolcat
+echo  "[AUTHOR    : MR.NOTHING] "                                   | lolcat
+echo  "[VERSION   : V.1] "                                          | lolcat
+echo  "[TOTALS    : 15] "                                           | lolcat
+echo  "[THANKS TO : INDONESIAN TERMUX ASSOCIATION] "                | lolcat
+echo  "[THANKS TO : INDO GHOST TEAM] "                              | lolcat
+echo  "[THANKS TO : CYBER FAMILY ASSOCIATION] "                     | lolcat
+echo  "[THANKS TO : HACKTIVITS INDONESIA]"                          | lolcat
+echo  "[THAMKS TO : BL4CK S3CR3T SQUAD]"                            | lolcat
+echo  "[THANKS TO : MR. UNKNOWN && 25 JUL] "                        | lolcat
+echo  "[NOTES     : MAAF KLO TOOLS MASIH ADA KEKURANGAN]"           | lolcat
+echo  "★=========================«[+]»==========================★"  | lolcat
+echo ""
+toilet -f slant --gay "MR.NOTHING"
+echo $b"★★★★★★★★★★★★★★★WELCOME TO TOOLS MR.NOTHING★★★★★★★★★★★★★★★" | lolcat
+sleep 1
+echo "[+]=====================================================[+]" | lolcat
+echo "[1] LITESPAM                                               " | lolcat
+echo "[+]=====================================================[+]" | lolcat
+echo "[2] LITESCRIPT                                             " | lolcat
+echo "[+]=====================================================[+]" | lolcat
+echo "[3] MBF                                                    " | lolcat
+echo "[+]=====================================================[+]" | lolcat
+echo "[4] WIFI-HACKER                                            " | lolcat
+echo "[+]=====================================================[+]" | lolcat
+echo "[5] INSTALL TOLLS MR.CAKIL                                 " | lolcat
+echo "[+]=====================================================[+]" | lolcat
+echo "[6] FB BRUTE                                               " | lolcat
+echo "[+]=====================================================[+]" | lolcat
+echo "[7] MENGUBAH TAMPILAN TERMUX                               " | lolcat
+echo "[+]=====================================================[+]" | lolcat
+echo "[8] SCAUTO                                                 " | lolcat
+echo "[+]=====================================================[+]" | lolcat
+echo "[9] TEMBAK PAKETAN                                         " | lolcat
+echo "[+]=====================================================[+]" | lolcat
+echo "[10] TU4N N1CKN4M3 TOOLSv2                                 " | lolcat
+echo "[+]=====================================================[+]" | lolcat
+echo "[11] INSTALL TOOLS TUANB4DUTv4                             " | lolcat
+echo "[+]=====================================================[+]" | lolcat
+echo "[12] AOCDEFACE                                             " | lolcat
+echo "[+]=====================================================[+]" | lolcat
+echo "[13] INSTALL TOOLS BAJINGANv6                              " | lolcat
+echo "[+]=====================================================[+]" | lolcat
+echo "[14] SC CREATOR                                            " | lolcat
+echo "[+]=====================================================[+]" | lolcat
+echo "[15] TES KECEPATAN JARINGAN                                " | lolcat
+echo "[+]=====================================================[+]" | lolcat
+echo "[00] EXIT                                                  " | lolcat
+echo "[+]=====================================================[+]" | lolcat
+read -p "«[+]silahkan pilih[+]»" gg;
+
+if [ $gg = 1 ] || [ $gg = 1 ]
+then
+apt update -y
+apt upgrade -y
+pkg install toilet
+pkg install php
+pkg install git
+pkg python2 -y
+git clone https://github.com/4L13199/LITESPAM
+cd LITESPAM
+sh LITESPAM.sh
+fi
+
+if [ $gg = 2 ] || [ $gg = 2 ]
+then
+apt update -y
+apt upgrade -y
+pkg install python2 git
+git clone https://github.com/4L13199/LITESCRIPT
+cd LITESCRIPT
+python2 LITESCRIPT.py
+fi
+
+if [ $gg = 3 ] || [ $gg = 3 ]
+then
+apt update && apt upgrade -y
+pkg install python2 -y
+pip2 install mechanize
+pip2 install --upgrade pip
+apt install git
+git clone https://github.com/hnov7/mbf
+cd mbf
+python2 mbf.py
+fi
+
+if [ $gg = 4 ] || [ $gg = 4 ]
+then
+apt update -y
+apt upgrade -y
+lone https://github.com/esc0rtd3w/wifi-hacker
+cd wifi-hacker
+sh wifi-hacker.sh
+fi
+
+if [ $gg = 5 ] || [ $gg = 5 ]
+then
+apt update && apt upgrade -y
+apt install git
+git clone https://github.com/mrcakil/Mrcakil.git
+cd Mrcakil
+chmod +x tools
+./tools
+fi
+
+if [ $gg = 6 ] || [ $gg = 6 ]
+then
+pkg update && pkg upgrade -y
+pkg install python2 -y
+pip2 install mechanize
+pkg install git
+git clone https://github.com/Senitopeng/KumpulanFbbrute.git
+cd KumpulanFbbrute
+python2 jomblo.py
+fi
+
+if [ $gg = 7 ] || [ $gg = 7 ]
+then
+pkg update && pkg upgrade -y
+pkg install ruby cowsay toilet figlet
+pkg install neofetch
+pkg install nano
+gem install lolcat
+cd ../usr/etc
+nano bash.bashrc
+fi
+
+if [ $gg = 8 ] || [ $gg = 8 ]
+then
+pkg update && pkg upgrade -y
+pkg install git
+git clone https://github.com/mrmsdv/Scauto.git
+cd Scauto
+python2 waluh.py
+fi
+
+if [ $gg = 9 ] || [ $gg = 9 ]
+then
+pkg install python
+pkg install git
+git clone https://github.com/kumpul4semut/semut.git
+cd semut
+python dor.py
+fi
+
+if [ $gg = 10 ] || [ $gg = 10 ]
+then
+apt update && apt upgrade -y
+pkg install python python2 vim figlet curl -y
+pip2 install lolcat
+pkg install git
+git clone https://github.com/TU4NN1CKN4M3/TOOLSv2
+cd TOOLSv2
+sh TU4NN1CKN4M3.sh
+fi
+
+if [ $gg = 11 ] || [ $gg = 11 ]
+then
+apt update && apt upgrade -y
+pkg install python python2 vim figlet curl -y
+pkg install php
+pip2 install lolcat
+pkg install git
+git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv4
+cd TOOLSINSTALLERv4
+chmod 777 TUANB4DUT..sh
+./TUANB4DUT..sh
+fi
+
+if [ $gg = 12 ] || [ $gg = 12 ]
+then
+apt update -y
+apt upgrade -y
+apt install git -y
+git clone https://github.com/Amriez/AOCDEFACE.git
+cd AOCDEFACE
+sh install.sh
+sh AOC.sh
+fi
+
+if [ $gg = 13 ] || [ $gg = 13 ]
+then
+apt install update -y
+apt install upgrade -y
+pip2 install termcolor
+pip2 install lolcat
+apt install toilet
+apt install git
+git clone https://github.com/DarknessCyberTeam/BAJINGANv6
+cd BAJINGANv6
+sh BAJINGAN.sh
+fi
+
+if [ $gg = 14 ] || [ $gg = 14 ]
+then
+apt update -y
+apt upgrade -y
+pkg install git
+pkg install python2 -y
+git clone https://github.com/cortexID/SCreator
+cd SCreator
+python2 SCreator.py
+fi
+
+if [ $gg = 15 ] || [ $gg = 15 ]
+then
+apt update && apt upgrade -y
+pkg install git
+pkg install requests
+pkg install python2 -y
+pip install --upgrade pip
+git clone https://github.com/Rusmana-ID/rus
+cd rus
+python2 Speed.py
+fi
+
+if [ $gg = 00 ] || [ $gg = 00 ]
+then
+echo "\033[32;1mTHANKS SUDAH MEMGGUNAKAN TOOLS GUA"|lolcat
+echo "JANGAN LUPA SHARE"|lolcat
+figlet "MR. NOTHING"|lolcat
+exit
+fi
